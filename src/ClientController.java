1f75cd81cca8bb85ddcab771146b23efc1224435
Celeste Rock <crock2@saddleback.edu>
2024-04-17 21:11:07 -0700

    adding server files

--- src/ClientController.java
+++ src/ClientController.java
@@ -17,6 +17,12 @@ public class ClientController {
     @FXML
     private Button sendBtn;
 
+    @FXML
+    private Button connectBtn;
+
+    @FXML
+    private Button disconnectBtn;
+
     private Client client;
 
     // Method to set the Client instance
@@ -30,22 +36,35 @@ public class ClientController {
     public void setupBindings() {
         sendBtn.disableProperty().bind(client.readyProperty().not());
     }
-    
+
     @FXML
     void sendMsgAction(ActionEvent event) {
         // Save whatever was typed into the sendMsgField
 
         try {
-            String message = msgInput.getText();   
+            String message = msgInput.getText();
             // call the sendMessage method of the Client instance
             client.sendMessage(message);
         }
         catch (Exception e) {
             System.out.println("Issue getting msgInput!!");
         }
-  
+
         // clear the input field after sending the message
         msgInput.clear();
+
+    }
+
+    @FXML
+    void handleConnect(ActionEvent event) {
+        //retrieve the server IP and port number from the GUI and
+        //use these fields to connect to the server
+
+    }
+
+    @FXML
+    void handleDisconnect(ActionEvent event) {
+        //initiate disconnecting from the server
 
     }
 
